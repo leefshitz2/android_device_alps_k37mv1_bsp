2082943776d524b55aed7a4b985dcda807dcb1c9
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-01-06 17:43:27 +0100

    k37mv1_bsp: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: alps Model: k37mv1_bsp Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for alps k37mv1_bsp (k37mv1_bsp)
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_k37mv1_bsp.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from k37mv1_bsp device
+$(call inherit-product, device/alps/k37mv1_bsp/device.mk)
+
+PRODUCT_DEVICE := k37mv1_bsp
+PRODUCT_NAME := omni_k37mv1_bsp
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := k37mv1_bsp
+PRODUCT_MANUFACTURER := alps
+
+PRODUCT_GMS_CLIENTID_BASE := android-alps
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_k37mv1_bsp-user 10 QP1A.190711.020 p1k61v164bspP15 release-keys"
+
+BUILD_FINGERPRINT := Kdoce/Z8/Z8:10/QP1A.190711.020/12573:user/release-keys

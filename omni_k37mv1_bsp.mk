3e386ef3fd97a6078a2f10d62f0a7d77ed172c39
leefshitz2 <leefshitz2@gmail.com>
2025-03-15 06:28:46 +0200

    Update omni_k37mv1_bsp.mk

--- omni_k37mv1_bsp.mk
+++ omni_k37mv1_bsp.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from k37mv1_bsp device
 $(call inherit-product, device/alps/k37mv1_bsp/device.mk)
